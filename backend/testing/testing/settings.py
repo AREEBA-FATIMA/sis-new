2469691383e40111cb183c0b1dc0994d77fd43fc
AREEBA-FATIMA <areebafatima2457@gmail.com>
2025-09-29 17:58:41 +0500

    centralized databse

--- backend/testing/testing/settings.py
+++ backend/testing/testing/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'testing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'school_db',         # Yeh database name hai jo upar bana
-#         'USER': 'postgres',          # Default user
-#         'PASSWORD': 'root8@',    # Apna password daal dena (jo install ke waqt set kiya)
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sis_two',         
+        'USER': 'postgres',          # Default user
+        'PASSWORD': 'password',    
+        'HOST': 'localhost',      # Default host
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
